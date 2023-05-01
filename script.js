ebb0a0b902216f6ae4365afed2ba95a4c4ac1e8d
Pranav-git <117561846+Pranavd-git@users.noreply.github.com>
2023-05-01 14:37:33 +0530

    Add return value for getComputerChoice()

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ function getComputerChoice(){
     //This variable will give random array index
     let randomChoice = Math.floor(Math.random()*choices.length);
     //This will return array value from above random array index 
+    choices = choices[randomChoice];
+    return choices;
 }
